9df4f274734d98f7de0d3edec39e973c12081f50
Ajay.K <ajaykundnani03@gmail.com>
2023-03-26 13:51:56 +0530

    New React Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import logo from './logo.svg'; 
 import './App.css';
-
+import Header from './Components/Header'
+import Hero from './Components/hero'
+import About from './Components/About'
+import Resume from './Components/Resume'
+import Portfolio from './Components/Portfolio'
+import Service from './Components/Service'
+import Contact from './Components/Contact'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header></Header>
+        <Hero></Hero>
+        <main id="main">
+        <About></About>
+        <Resume></Resume>
+        <Portfolio></Portfolio>
+        <Service></Service>
+        <Contact></Contact>
+        </main>
       </header>
     </div>
   );
---
+++ src/Components/hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+export default class Hero extends React.Component{
+    render()
+    {
+        return(
+            <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+            <div class="hero-container" data-aos="fade-in">
+              <h1>Ajay Kundnani</h1>
+            </div>
+          </section>
+        
+        )
+    }
+ }
